a78a46e96880cede76674e878264a0137811127f
Vu Pham <phvu225@gmail.com>
2012-05-17 16:02:57 +0200

    Added some start-up code.

---
+++ constructGraph.m
@@ -0,0 +1,12 @@
+function [ output_args ] = constructGraph( input_args )
+%CONSTRUCTGRAPH Construct a bipartite users-items graph.
+%   This function will create the graph with all information.
+%   The result is a sparse matrix represent this graph, which 
+%   then will be used with MatlabBGL.
+
+% maybe after construct the graph,
+% we will store the graph into a *.mat file,
+% to avoid creating the graph again and again.
+
+end
+
---
+++ run.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% run the algorithm
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% initialize
+init;
+
+% load the graph
+% blah blah
+
+% run shortest path using MatlabBGL
+% blah blah
+
+% do some stuff...
