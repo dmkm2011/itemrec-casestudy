739e5a7eeace6fa1553896d8273d3338a270e468
Vu Pham <phvu225@gmail.com>
2012-05-30 01:27:10 +0200

    minor changes, added some functions without body.

---
+++ computeUserDistance.m
@@ -0,0 +1,7 @@
+function [ output_args ] = computeUserDistance( users, i, j )
+%COMPUTEUSERDISTANCE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- constructGraph.m
+++ constructGraph.m
@@ -1,4 +1,4 @@
-function [ output_args ] = constructGraph( input_args )
+function [ G ] = constructGraph( users, items )
 %CONSTRUCTGRAPH Construct a bipartite users-items graph.
 %   This function will create the graph with all information.
 %   The result is a sparse matrix represent this graph, which 
@@ -8,5 +8,30 @@ function [ output_args ] = constructGraph( input_args )
 % we will store the graph into a *.mat file,
 % to avoid creating the graph again and again.
 
+    numUsers = length(users.userIds);
+    numItems = length(items.itemIds);
+    numVertices = numUsers + numItems;
+    
+    rowIndices = zeros(3*numVertices, 1);
+    colIndices = zeros(3*numVertices, 1);
+    edgeValues = zeros(3*numVertices, 1);
+    
+    % user-user distance
+    for i=1:numUsers
+        for j=i+1:numUsers
+            computeUserDistance(users, i, j);
+        end
+    end
+    
+    % item-item distance
+    for i=1:numItems
+        for j=i+1:numItems
+            computeItemDistance(items, i, j);
+        end
+    end
+    
+    % user-item 
+    
+    G = sparse(rowIndices, colIndices, edgeValues);
 end
 
